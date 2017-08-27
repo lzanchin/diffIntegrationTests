61302983af17c8a3f602c3885d865560b0f3cc64
Leandro Augusto Zanchin <leandro.zanchin@gmail.com>
2017-08-27 19:59:16 -0300

    Add project files.

---
+++ DiffIntegrationTests/DiffIntegrationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+
+namespace DiffIntegrationTests
+{
+    [TestClass]
+    public class DiffIntegrationTest
+    {
+        [TestMethod]
+        public void RequestShouldRespond()
+        {
+            var client = new HttpClient();
+
+            var content = new StringContent("Some text");
+            var response = client.PostAsync("http://localhost:49591/v1/diff/1/left", content).Result;
+
+            //Assert.AreEqual("Some text", response.Content.ReadAsStringAsync().Result);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ DiffIntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DiffIntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiffIntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b4bdab88-4192-497f-ab83-e9715d7ae995")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
